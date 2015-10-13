e6bcf8b0ed0b50d8b3dfc984a3169c485ae300ab
Paul Broadwith <pauby@users.noreply.github.com>
2015-10-13 18:13:11 +0100

    Added function Get-Todo which gets all todos and returns them. This provides continuity with standard PowerShell and as we have Set-Todo.

---
+++ Get-Todo.ps1
@@ -0,0 +1,31 @@
+﻿<# 
+.SYNOPSIS
+    Gets the todos.
+.DESCRIPTION
+    Gets the todos from the todo file.
+.NOTES 
+    Author		: Paul Broadwith (paul@pauby.com)
+	History		: 1.0 - 13/10/15 - Initial version
+.LINK 
+    http://www.github.com/pauby
+.PARAMETER Path
+    The path of the todo file. By default this is the TodoTaskFile from the confguration.
+.OUTPUTS
+	None
+.EXAMPLE
+    Get-Todo
+		
+	Gets the todos from the todo task file in the configuration.
+#>
+
+function Get-Todo
+{
+    [CmdletBinding()]
+    Param (
+        [string]$Path= (Get-TodoConfig)['TodoTaskFile']
+    )
+    
+    $todos = import-todo $Path -Verbose:$VerbosePreference
+
+    ,$todos
+}
