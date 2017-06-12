9f8c4fe57e237572a3cbd81e32b88e23a3477b2d
Paul Broadwith <paul@pauby.com>
2017-06-12 11:11:34 +0100

    Updated tests

--- Tests/Public/Import-Todo.Tests.ps1
+++ Tests/Public/Import-Todo.Tests.ps1
@@ -1,6 +1,12 @@
 $ourModule = 'PSTodoWarrior'
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$projRoot = Split-Path -Parent $here
+if ($here -match ".*pstodowarrior") {
+    $projRoot = $matches[0]
+}
+else {
+    throw "Cannot find project root folder."
+}
+
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 <#
 $functionName = $sut -replace '\.ps1'
@@ -21,38 +27,35 @@ else {
 Remove-Module $ourModule -ErrorAction SilentlyContinue
 Import-Module (Join-Path -Path $projRoot -ChildPath $ourModule) -Force
 
-#InModuleScope -ModuleName $ourModule {
-
-    Describe "Script Testing" {
-        Context "Parameter Validation" {
-            It "Should throw an exception for an empty path" {
-                { Import-Todo -Path ''  } | Should throw "Cannot validate argument on parameter 'Path'"
-            }
-
-            It "Should throw an exception if the todo file does not exist" {
-                { Import-Todo -Path 'TestDrive:missing.txt' } | Should throw "Cannot validate argument on parameter 'Path'"
-            }
+Describe "Script Testing" {
+    Context "Parameter Validation" {
+        It "Should throw an exception for an empty path" {
+            { Import-Todo -Path ''  } | Should throw "Cannot validate argument on parameter 'Path'"
         }
 
-        Context "Processing and Logic" {
+        It "Should throw an exception if the todo file does not exist" {
+            { Import-Todo -Path 'TestDrive:missing.txt' } | Should throw "Cannot validate argument on parameter 'Path'"
+        }
+    }
 
-            Mock -ModuleName PsTodoWarrior Import-TodoTxt { @( [pscustomobject]@{ task = "abc"}, [pscustomobject]@{ task = "def"} ) } -Verifiable
+    Context "Processing and Logic" {
+    }
 
-            It 'Adds a line property to each todo' {
-                # need to create an empty file as the function tests for this 
-                Set-Content -Path 'TestDrive:todo.txt' -Value "."
-                $todo = Import-Todo -Path 'TestDrive:todo.txt'
-                $todo.count | Should Be 2
-                for ($i = 0; $i -lt $todo.count; $i++) {
-                    $todo[$i].Line | Should be ($i+1) 
-                }
-                { Assert-VerifiableMocks } | Should not throw
+    Context "Output" {
+        Mock -ModuleName PsTodoWarrior Import-TodoTxt { @( [pscustomobject]@{ task = "abc"}, [pscustomobject]@{ task = "def"} ) } -Verifiable
+
+        It 'Adds a line property to each todo' {
+            # need to create an empty file as the function tests for this 
+            Set-Content -Path 'TestDrive:todo.txt' -Value "."
+            $todo = Import-Todo -Path 'TestDrive:todo.txt'
+            $todo.count | Should Be 2
+            $result = @( [pscustomobject]@{ task = "abc"; line = 1}, [pscustomobject]@{ task = "def"; line = 2} )
+            Compare-Object -ReferenceObject $todo -DifferenceObject $result | Should Be $null
+            for ($i = 0; $i -lt $todo.count; $i++) {
+                $todo[$i].Line | Should be ($i + 1) 
             }
+            { Assert-VerifiableMocks } | Should not throw
         }
-
-        Context "Output" {
-            # the output comes from the ConvertFrom-TodoTxtString function. We tested this has been
-            # called above so no further output tests needed
-        }
-#    }
+    }
 }
+
