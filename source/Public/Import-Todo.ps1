db856007423caf8412c662c53ad9679d65a49737
Paul Broadwith <paul@pauby.com>
2017-11-08 12:56:51 +0000

    Old functions - updated

--- source/Public/Import-Todo.ps1
+++ source/Public/Import-Todo.ps1
@@ -1,33 +1,37 @@
 ﻿function Import-Todo
 {
-    <# 
-    .SYNOPSIS
-        Imports todos from the todo file.
-    .DESCRIPTION
-        Imports the todos from the todo file and then adds a line number to the todo object..
-    .LINK 
-        https://www.guthub.com/pauby/PSTodoWarrior            
-    .PARAMETER Path
-        Path to the todo file. Default is TodoTaskFile from the mofule configuration.
-    .OUTPUTS
-        Output is [array]
-    .EXAMPLE
-        Import-Todo c:\todo.txt
+<#
+.SYNOPSIS
+    Imports todos from the todo file.
+.DESCRIPTION
+    Imports the todos from the todo file and then adds a line number to the todo object..
+.PARAMETER Path
+    Path to the todo file. Default is TodoTaskFile from the mofule configuration.
+.OUTPUTS
+    System.ArrayList
+.EXAMPLE
+    Import-Todo c:\todo.txt
 
-        Read and outputs the todos from c:\todo.txt
-    #>
+    Read and outputs the todos from c:\todo.txt
+.LINK
+    https://www.guthub.com/pauby/PSTodoWarrior
+.NOTES
+    Author: Paul Broadwith (https://pauby.com)
+#>
     Param (
+        [Parameter(Mandatory = $true, Position = 0,
+            ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
         [ValidateScript( { Test-Path $_ } )]
         [ValidateNotNullOrEmpty()]
-        [string]$Path = $todoConfig.TodoTaskFile
+        [string]$Path = $script:twSettings.TodoTaskFile
     )
 
     Write-Verbose "Importing todos and adding the line number property."
     Import-TodoTxt -Path $Path | ForEach-Object `
-        -Begin { 
-            $count = 1 
+        -Begin {
+            $count = 1
         } `
-        -Process { 
+        -Process {
             $_ | Add-Member -MemberType NoteProperty -Name 'Line' -Value $count -Passthru
             $count++
         }
--- source/Public/Use-Todo.ps1
+++ source/Public/Use-Todo.ps1
@@ -1,13 +1,13 @@
 ﻿function Use-Todo
 {
-<# 
+<#
 .SYNOPSIS
     One overall function to work with todos.
 .DESCRIPTION
     This cmdlet is a wrapper for the other cmdlets and allows you to work with todos..
 
 .PARAMETER List
-    A command switch to list the todos on the host. This is also the default when no other command switch is provided.
+    List / show the todos on the host. This is also the default when no other command switch is provided.
 .EXAMPLE
     Use-Todo -command add -priority A -task 'Take car to the garage' -project 'care-maintenance' -context 'car'
 
@@ -16,7 +16,7 @@
     Use-Todo remove 15
 
     Removes the todo at line 15.
-.LINK 
+.LINK
     http://www.github.com/pauby/pstodowarrior
 .NOTES
     Author: Paul Broadwith (https://pauby.com)
