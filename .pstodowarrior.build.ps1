d921a60ea711dcd0b46f25d7b63b7b28c6180546
Paul Broadwith <paul@pauby.com>
2019-02-07 18:54:03 +0000

    Replaced build script

--- .pstodowarrior.build.ps1
+++ .pstodowarrior.build.ps1
@@ -1,23 +1,41 @@
-[CmdletBinding()]
-Param (
-    [String]
-    $ReleaseType = 'Unknown',
+Import-Module PowerShellBuild -force
+. PowerShellBuild.IB.Tasks
 
-    [string]
-    $GitHubUsername = $env:GITHUB_USERNAME,
+$PSBPreference.Build.CompileModule = $true
+# $PSBPreference.Build.Dependencies                           = 'StageFiles', 'BuildHelp'
+$PSBPreference.Test.Enabled = $true
+$PSBPreference.Test.CodeCoverage.Enabled = $true
+$PSBPreference.Test.CodeCoverage.Threshold = 0.75
+$PSBPreference.Test.CodeCoverage.Files =
+(Join-Path -Path $PSBPreference.Build.ModuleOutDir -ChildPath "*.psm1")
+$PSBPreference.Test.ScriptAnalysis.Enabled = $true
+$PSBPreference.Test.ScriptAnalysis.FailBuildOnSeverityLevel = 'Error'
 
-    [string]
-    $GitHubApiKey = $env:GITHUB_API_KEY,
+task LocalDeploy {
+    $sourcePath = $PSBPreference.Build.ModuleOutDir
+    $destPath = Join-Path -Path ([Environment]::GetFolderPath('MyDocuments')) `
+        -ChildPath "WindowsPowerShell\Modules\$($PSBPreference.General.ModuleName)\$($PSBPreference.General.ModuleVersion)\"
 
-    [string]
-    $PSGalleryApiKey = $env:PSGALLERY_API_KEY
-)
-
-$params = @{
-    ReleaseType     = $ReleaseType
-    GitHubUserName  = $GitHubUsername
-    GitHubApiKey    = $GitHubApiKey
-    PSGalleryApiKey = $PSGalleryApiKey
+    if (Test-Path -Path $destPath) {
+        Remove-Item -Path $destPath -Recurse -Force
+    }
+    Copy-Item -Path $sourcePath -Destination $destPath -Recurse -Force
 }
 
-. (Get-BuildScript) @params
+$moduleVersion = (Get-Module -Name PowerShellBuild -ListAvailable | Sort-Object Version -Descending | Select-Object -First 1).Version
+if ($moduleVersion -le [version]"0.3.0") {
+    task Build {
+        #Write-Host "Setting env"
+        #[Environment]::SetEnvironmentVariable("BHBuildOutput", $PSBPreference.Build.ModuleOutDir, "machine")
+    }, StageFiles, BuildHelp
+
+    task Init {
+        Initialize-PSBuild
+        Set-BuildEnvironment -BuildOutput $PSBPreference.Build.ModuleOutDir -Force
+        $nl = [System.Environment]::NewLine
+        "$nl`Environment variables:"
+        (Get-Item ENV:BH*).Foreach( {
+                '{0,-20}{1}' -f $_.name, $_.value
+            })
+    }
+}
