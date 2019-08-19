e40ce7c3450ac3ee70a07bb9552fa98b77dabb85
Paul Broadwith <paul@pauby.com>
2019-08-19 22:55:51 +0100

    Return imported todos and use imported settings

--- source/Public/Import-TWTodo.ps1
+++ source/Public/Import-TWTodo.ps1
@@ -13,9 +13,6 @@
         Read and outputs the todos from c:\todo.txt
     .NOTES
         Author:  Paul Broadwith (https://pauby.com)
-        Project: PSTodoWarrior (https://github.com/pauby/pstodowarrior)
-        History: 1.0 - unknown - initial release
-                 1.1 - 09/07/18 - Renamed function; Updated comments
     .LINK
         https://www.github.com/pauby/pstodowarrior/tree/master/docs/import-twtodo.md
     #>
@@ -27,31 +24,25 @@
         [Parameter(Position = 0, ValueFromPipeline, ValueFromPipelineByPropertyName)]
         [ValidateScript( { Test-Path $_ } )]
         [string]
-        $Path = $global:TWSettings.TodoTaskPath,
-
-        # Returns the TWTodo object list created
-        [switch]
-        $PassThru
+        $Path = ((Get-TWSettings).TodoTaskPath)
     )
 
-    Write-Verbose "Checking that todo file '$($global:TWSettings.TodoTaskPath)' exists."
-    If (Test-Path -Path $global:TWSettings.TodoTaskPath) {
+    Write-Verbose "Checking that todo file '$($Path)' exists."
+    If (Test-Path -Path $Path) {
         Write-Verbose "Importing todos and adding an ID number to each."
-        [System.Collections.ArrayList]$script:TWTodo = Import-TodoTxt -Path $global:TWSettings.TodoTaskPath | ForEach-Object `
-            -Begin {
-                $count = 1
-            } `
-            -Process {
-                $_.PSObject.TypeNames.Insert(0, 'TWTodo')
-                $_ | Add-Member -MemberType NoteProperty -Name 'ID' -Value $count -PassThru
-                $count++
-            }
-
-        if ($PassThru.IsPresent) {
-            $script:TWTodo
+        [System.Collections.ArrayList]$script:TWTodo = Import-TodoTxt -Path $Path | ForEach-Object `
+        -Begin {
+            $count = 1
+        } `
+        -Process {
+            $_.PSObject.TypeNames.Insert(0, 'TWTodo')
+            $_ | Add-Member -MemberType NoteProperty -Name 'ID' -Value $count -PassThru
+            $count++
         }
     }
     else {
-        Write-Error "Todo file '$($global:TWSettings.TodoTaskPath) does not exist!"
+        Write-Error "Todo file '$($Path) does not exist!"
     }
+
+    $script:TWTodo
 }
