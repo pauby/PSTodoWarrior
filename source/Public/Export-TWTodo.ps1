5b553e4d25d568c973fabcc5a84ce551c249a35c
Paul Broadwith <paul@pauby.com>
2020-02-16 15:26:07 +0000

    Add backup todo file before modifying it

--- source/Public/Export-TWTodo.ps1
+++ source/Public/Export-TWTodo.ps1
@@ -28,27 +28,74 @@
     # get the config
     $config = Get-TWConfiguration
 
-    Write-Verbose "Exporting todos."
-
-    # check if we have completed todos and autoarchiving is enabled
-    $toBeExported = $Todo      # by default ALL todos are exported to the task file
-    if ($config.AutoArchive -eq $true -and $config.TodoDonePath) {
-        Write-Verbose "Autoarchiving of completed todo(s) is enabled."
-        $completed = $Todo | Where-Object { [string]::IsNullOrEmpty($_.DoneDate) -eq $false }
-        Write-Verbose "Found $(@($completed).count) completed todo(s)."
-
-        # if we have todos that have a completed value then export them to the done file
-        if ($completed) {
-            Write-Verbose "Exporting $(@($completed).count) completed todo(s) to '$($config.TodoDonePath)'."
-            $completed | Export-TodoTxt -Path $config.TodoDonePath -Append
+    # determine if there is a configuration for backing up the todo file before
+    # we modify it - if not, still back it up and remove it when we are finished
+    $backupFilename = "{0}-{1}" -f (Get-Date -Format "yyyymmdd-HHMMss"), (Split-Path -Path $config.TodoTaskPath -Leaf)
+    if ($config.BackupPath) {
+        # before we do anything check that the pathfor the backups exists
+        if (-not (Test-Path -Path $config.BackupPath)) {
+            # path does not exist so lets try and create it
+            try {
+                Write-Verbose "The 'BackupPath' given in the configuration does not exist. Trying to create '$($config.BackupPath)'."
+                New-Item -Path $config.BackupPath -ItemType Directory -Force
+            }
+            catch {
+                throw "Path '$($config.BackupPath)' does not exist. Tried to create it but failed."
+            }
         }
-
-        # remove the completed todos from our current list
-        $toBeExported = $Todo | Where-Object { [string]::IsNullOrEmpty($_.DoneDate) -eq $true }
+        $backupPath = Join-Path -Path $config.BackupPath -ChildPath $backupFilename
+    }
+    else {
+        $backupPath = Join-Path -Path $taskPath -ChildPath $backupFilename
     }
 
-    # we dont' need to striup off any of the TodoWarrior extra fields as they
-    # will simply be ignored when exporting
-    Write-Verbose "Exporting $(@($toBeExported).count) todo(s) to '$($config.TodoTaskPath)'."
-    $toBeExported | Export-TodoTxt -Path $config.TodoTaskPath
+    # make a copy of the todo file
+    try {
+        Write-Verbose "Trying to make a copy of the todo file '$($config.TodoTaskPath)' to '$backupPath'."
+        Copy-Item -Path $config.TodoTaskPath -Destination $backupPath
+    }
+    catch {
+        throw "Before modifying the todo file '$($config.TodoTaskPath)' I tried to make a copy to '$backupPath' but I failed."
+    }
+
+    Write-Verbose "Going to export todos."
+
+    try {
+        # check if we have completed todos and autoarchiving is enabled
+        $toBeExported = $Todo      # by default ALL todos are exported to the task file
+        if ($config.AutoArchive -eq $true -and $config.TodoDonePath) {
+            Write-Verbose "Autoarchiving of completed todo(s) is enabled."
+            $completed = $Todo | Where-Object { [string]::IsNullOrEmpty($_.DoneDate) -eq $false }
+            Write-Verbose "Found $(@($completed).count) completed todo(s)."
+
+            # if we have todos that have a completed value then export them to the done file
+            if ($completed) {
+                Write-Verbose "Exporting $(@($completed).count) completed todo(s) to '$($config.TodoDonePath)'."
+                $completed | Export-TodoTxt -Path $config.TodoDonePath -Append
+            }
+
+            # remove the completed todos from our current list
+            $toBeExported = $Todo | Where-Object { [string]::IsNullOrEmpty($_.DoneDate) -eq $true }
+        }
+
+        # we don't need to strip off any of the TodoWarrior extra fields as they
+        # will simply be ignored when exporting
+        Write-Verbose "Exporting $(@($toBeExported).count) todo(s) to '$($config.TodoTaskPath)'."
+        $toBeExported | Export-TodoTxt -Path $config.TodoTaskPath
+    }
+    catch {
+        # if something has gone wrong lets throw, warn the user and let them fix whatever it was
+        throw "There was an error exporting to the todo file '$($config.TodoTaskPath)'. A backup was made beforehand and it is at '$backupPath' which you can use if the todo file is in an inconsistent state."
+    }
+
+    if (-not $config.BackupPath) {
+        Write-Verbose "Everything went okay with the export so removing the todo backup file '$backupPath'."
+        try {
+            Remove-Item -Path $backupPath
+        }
+        catch {
+            # lets not make this a terminating error. Just warn the user and move on.
+            Write-Warning "I tried to remove the backup todo file at '$backupPath' but failed. It is still there and needs to be removed manually."
+        }
+    }
 }
