cb074ed9eb0db34663c3aadc2f13b184cc72eeaa
Paul Broadwith <pauby@users.noreply.github.com>
2020-02-16 22:37:04 +0000

    Update help and logic

--- source/Public/Export-TWTodo.ps1
+++ source/Public/Export-TWTodo.ps1
@@ -2,25 +2,32 @@
 {
     <#
     .SYNOPSIS
-        Imports todos from the todo file.
+        Exports todos.
     .DESCRIPTION
-        Imports the todos from the todo file and then adds a line number to the todo object.
-    .OUTPUTS
-        System.ArrayList
-    .EXAMPLE
-        Export-TWTodo c:\todo.txt
+        Exports the todos to the todo list file and optionally the todo done
+        file. Todos are exported to the done file is the AutoArchive
+        configuration setting is defined.
 
-        Read and outputs the todos from c:\todo.txt
+        The todo list is backed up before any modifications are made to it. If
+        BackupPath has been set in the configuration the backups will be made to
+        that path and kept afterwards. IOf the BackupPath has not been defined
+        then a backup will still ba made but it will be removed if the export
+        was successful.
+    .EXAMPLE
+        Export-TWTodo -Todo $myTodo
+
+        Exports the todos in $myTodo to the todo and (optionally) the done files.
     .NOTES
         Author:  Paul Broadwith (https://pauby.com)
         Project: PSTodoWarrior (https://github.com/pauby/pstodowarrior)
-        History: 1.0 - 16/07/18 - Initial
     .LINK
         https://www.github.com/pauby/pstodowarrior/tree/master/docs/import-twtodo.md
     #>
-    [OutputType([System.Collections.ArrayList])]
     [CmdletBinding()]
     Param (
+        [Parameter(Mandatory)]
+        [ValidateNotNull()]
+        [AllowEmptyCollection()]
         [System.Collections.ArrayList]
         $Todo
     )
@@ -43,7 +50,6 @@
                 try {
                     Write-Verbose "The 'BackupPath' given in the configuration does not exist. Trying to create '$($config.BackupPath)'."
                     New-Item -Path $config.BackupPath -ItemType Directory -Force -ErrorAction Stop
-                    Write-Verbose "we created this"
                 }
                 catch {
                     throw "Path '$($config.BackupPath)' does not exist. Tried to create it but failed."
@@ -57,32 +63,6 @@
             $todoTaskPathParent = Split-Path -Path $config.TodoTaskPath -Parent
             $backupPath = Join-Path -Path $todoTaskPathParent -ChildPath $backupFilename
         }
-        $backupPath = Join-Path -Path $config.BackupPath -ChildPath $backupFilename
-    }
-    else {
-        $backupPath = Join-Path -Path $taskPath -ChildPath $backupFilename
-    }
-
-    # make a copy of the todo file
-    try {
-        Write-Verbose "Trying to make a copy of the todo file '$($config.TodoTaskPath)' to '$backupPath'."
-        Copy-Item -Path $config.TodoTaskPath -Destination $backupPath
-    }
-    catch {
-        throw "Before modifying the todo file '$($config.TodoTaskPath)' I tried to make a copy to '$backupPath' but I failed."
-    }
-
-    Write-Verbose "Going to export todos."
-
-    try {
-        # check if we have completed todos and autoarchiving is enabled
-        $toBeExported = $Todo      # by default ALL todos are exported to the task file
-        if ($config.AutoArchive -eq $true -and $config.TodoDonePath) {
-            Write-Verbose "Autoarchiving of completed todo(s) is enabled."
-            $completed = $Todo | Where-Object { [string]::IsNullOrEmpty($_.DoneDate) -eq $false }
-            Write-Verbose "Found $(@($completed).count) completed todo(s)."
-
-        Write-Verbose "Backup of todo file will be created at '$backupPath'"
 
         # make a copy of the todo file
         try {
@@ -107,7 +87,7 @@
             # if we have todos that have a completed value then export them to the done file
             if ($completed) {
                 Write-Verbose "Exporting $(@($completed).count) completed todo(s) to '$($config.TodoDonePath)'."
-                $completed | Export-TodoTxt -Path $config.TodoDonePath -Append
+                $completed | Export-TodoTxt -Path $config.TodoDonePath -Append -ErrorAction Stop
             }
 
             # remove the completed todos from our current list
@@ -116,12 +96,18 @@
 
         # we don't need to strip off any of the TodoWarrior extra fields as they
         # will simply be ignored when exporting
-        Write-Verbose "Exporting $(@($toBeExported).count) todo(s) to '$($config.TodoTaskPath)'."
-        $toBeExported | Export-TodoTxt -Path $config.TodoTaskPath
+        if ($toBeExported.Count -eq 0) {
+            Write-Verbose "No todos to export - emptying files '$($config.TodoTaskPath)'."
+            Set-Content -Path $config.TodoTaskPath -Value ''
+        }
+        else {
+            Write-Verbose "Exporting $(@($toBeExported).count) todo(s) to '$($config.TodoTaskPath)'."
+            $toBeExported | Export-TodoTxt -Path $config.TodoTaskPath -ErrorAction Stop
+        }
     }
     catch {
         # if something has gone wrong lets throw, warn the user and let them fix whatever it was
-        throw "There was an error exporting to the todo file '$($config.TodoTaskPath)'. A backup was made beforehand and it is at '$backupPath' which you can use if the todo file is in an inconsistent state."
+        throw "There was an error exporting to the todo file '$($config.TodoTaskPath)'. A backup was made beforehand and it is at '$backupPath' which you can use if the todo file is in an inconsistent state. (Error: $Error["
     }
 
     # if we don't have a backup folder specified AND if the todo file actually existed then remove the temporary file
