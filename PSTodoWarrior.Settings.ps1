1b1bc9355c10cbf71f8ef8443aad5e9cfdbaddc1
Paul Broadwith <paul@pauby.com>
2020-05-12 17:15:57 +0100

    Add new settings

--- PSTodoWarrior.Settings.ps1
+++ PSTodoWarrior.Settings.ps1
@@ -1,12 +1,16 @@
 @{
-    TodoTaskPath        = 'todo.txt';
-    TodoDonePath        = 'done.txt';
+    TodoTaskPath        = 'todo.txt'
+    TodoDonePath        = 'done.txt'
 
     # Context and project name
     # name text of the Context property of the todo - usually 'Context' or 'List'
-    NameContext         = 'Context';
+    NameContext         = 'Context'
     # name text of the Project property of the todo - usually 'Project' or 'Tag'
-    NameProject         = 'Project';
+    NameProject         = 'Project'
+
+    # Addons
+    # addons not to show (note they are still on the object)
+    HideAddons          = @( 'uuid' )
 
     # Archives
     # if $true automatically archives completed todos to the TodoDoneFile, if $false they remain in the TodoTaskFile
@@ -14,45 +18,47 @@
 
     # Backups
     # backups are stored in the same folder as the TodoTaskFile
-    BackupFolder        = 'backups';
+    BackupFolder        = 'backups'
     # Number of backups to keep in the BackupFolder
-    BackupDaysToKeep    = 7;
+    BackupDaysToKeep    = 7
 
     # Colours
     # Colours for each weights - any weight at or above the level will be that colour (up to the previous value).
     # This MUST be an ordered hashtable for it to work.
     # it's in the format 'weight number' = 'valid PowerShell colour'
     WeightForegroundColour = [ordered]@{
-        20 	= 'yellow';
-        15 	= 'red';
-        1 	= 'darkgreen';
+        20 	= 'yellow'
+        15 	= 'red'
+        1 	= 'darkgreen'
     };
 
 
-    # Colour for information messages
-    InfoMsgsColour         = 'DarkCyan';
+    # Colours
+    # Text colour for every second line
+    ShowAlternatingColour  = 'DarkMagenta'
+    InfoMsgsColour         = 'DarkCyan'
 
 
     # Weights
     # TODO: These needs explained
-    WeightPriority    = 6.0;
-    WeightDueDate     = 12.0;
-    WeightHasProject  = 1.0;
-    WeightAge         = 2.0;
+    WeightPriority    = 6.0
+    WeightDueDate     = 12.0
+    WeightHasProject  = 1.0
+    WeightAge         = 2.0
     WeightProject     = @{                    # all projects / tags must be in lowercase
-        next    = 15.0;
-        waiting = -3.0;
-        today   = 20.0;
-        someday = -15.0;
+        next    = 15.0
+        waiting = -3.0
+        today   = 20.0
+        someday = -15.0
     };
 
     # Views
     # TODO: These need explained
-    TodoLimit   = 25;
+    TodoLimit   = 25
     Filter = @{
 #            'default' = { param([Parameter(ValueFromPipeline=$true)][object[]]$todos, [hashtable]$config); begin { $output = @() } process { foreach ($todo in $todos) { if (($todo.Project -contains $config['ProjectDefault']) -or ($todo.Priority) -or ($todo.Project -contains $config['ProjectNextAction'])) { $output += $todo } } } end { $output | where { [string]::IsNullOrWhitespace($_.DoneDate) } | Sort-Object -Property @{e="Weight"; Descending=$true}, @{e="Line"; Descending=$False} | Select-Object -First $config['TodoLimit'] } };
 #        default = { param([Parameter(ValueFromPipeline=$true)][object[]]$todos, [hashtable]$config); begin { $output = @() } process { foreach ($todo in $todos) { if (($todo.Context.Count -gt 0) -and ([string]::IsNullOrWhiteSpace($todo.DoneDate))) { $output += $todo } } } end { $output | where { [string]::IsNullOrWhitespace($_.DoneDate) } | Sort-Object -Property @{e="Weight"; Descending=$true}, @{e="Line"; Descending=$False} | Select-Object -First $config['TodoLimit'] } };
         all     = { param([Parameter(ValueFromPipeline=$true)][object[]]$todos, [hashtable]$config); begin { $output = @() } process { foreach ($todo in $todos) { $output += $todo } } end { $output | sort Weight -Descending } };
         inbox   = { param([Parameter(ValueFromPipeline=$true)][object[]]$todos, [hashtable]$config); begin { $output = @() } process { foreach ($todo in $todos) { if ($todo.Context.Count -eq 0) { $output += $todo } } } end { $output | where { [string]::IsNullOrWhitespace($_.DoneDate) } | Sort-Object -Property Weight -Descending } };
-    };
+    }
 }
